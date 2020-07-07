7994333a1d37c3358c3182b163896277584361c5
Solal Pirelli <solal.pirelli@gmail.com>
2020-07-07 13:34:06 +0200

    Rewrite map contract to make it ghost-friendly given lack of inference on pointers

--- tool/executors/binary/externals/os/structs/map.py
+++ tool/executors/binary/externals/os/structs/map.py
@@ -5,14 +5,13 @@ import executors.binary.utils as utils
 from collections import namedtuple
 
 
-# inductive map_item = map_item(void* key_addr, list<char> key, uint64_t value);
-# predicate mapp(struct os_map* map, uint64_t key_size, uint64_t capacity, list<map_item> items);
-Map = namedtuple('mapp', ['key_size', 'capacity', 'items_key_addrs', 'items_values'])
+# predicate mapp(struct os_map* map, uint64_t key_size, uint64_t capacity, list<pair<list<char>, uint64_t> > values, list<pair<void*, list<char> > > addrs);
+Map = namedtuple('mapp', ['key_size', 'capacity', 'values', 'addrs'])
 
 # struct os_map* os_map_init(uint64_t key_size, uint64_t capacity);
 # requires 0 < capacity &*& capacity <= MAP_CAPACITY_MAX &*&
 #          0 < key_size;
-# ensures result == 0 ? true : mapp(result, key_size, capacity, nil);
+# ensures result == 0 ? true : mapp(result, key_size, capacity, nil, nil);
 class MapInit(angr.SimProcedure):
   def run(self, key_size, capacity):
     # Casts
@@ -37,22 +36,22 @@ class MapInit(angr.SimProcedure):
     def case_false(state):
       print("!!! map init return nonzero")
       result = state.memory.allocate_opaque("os_map")
-      items_key_addrs = state.maps.allocate(key_size * 8, bitsizes.PTR, name="map_items_key_addrs") # key_size is in bytes
-      items_values = state.maps.allocate(key_size * 8, bitsizes.UINT64_T, name="map_items_values") # key_size is in bytes
-      state.metadata.set(result, Map(key_size, capacity, items_key_addrs, items_values))
+      values = state.maps.allocate(key_size * 8, bitsizes.UINT64_T, name="map_values") # key_size is in bytes
+      addrs = state.maps.allocate(bitsizes.PTR, key_size * 8, name="map_addrs") # key_size is in bytes
+      state.metadata.set(result, Map(key_size, capacity, values, addrs))
       return result
     return utils.fork_always(self, case_true, case_false)
 
 # bool os_map_get(struct os_map* map, void* key_ptr, uint64_t* value_out);
-# requires mapp(map, ?key_size, ?capacity, ?items) &*&
+# requires mapp(map, ?key_size, ?capacity, ?values, ?addrs) &*&
 #          chars(key_ptr, key_size, ?key) &*&
 #          *value_out |-> _;
-# ensures mapp(map, key_size, capacity, items) &*&
+# ensures mapp(map, key_size, capacity, values, addrs) &*&
 #         chars(key_ptr, key_size, key) &*&
 #         *value_out |-> ?value &*&
-#         switch(map_item_keyed(key, items)) {
+#         switch(map_item_keyed(key, values)) {
 #           case none: return result == false;
-#           case some(it): return result == true &*& it == map_item(_, key, value);
+#           case some(it): return result == true &*& it == value;
 #         };
 class MapGet(angr.SimProcedure):
   def run(self, map, key_ptr, value_out):
@@ -75,24 +74,25 @@ class MapGet(angr.SimProcedure):
     # Postconditions
     def case_has(state, item):
       print("!!! map get has", item)
-      state.add_constraints(state.maps.get(mapp.items_key_addrs, key)[1])
       state.mem[value_out].uint64_t = item
       return state.solver.BVV(1, bitsizes.BOOL)
     def case_not(state):
       print("!!! map get not")
-      state.add_constraints(state.solver.Not(state.maps.get(mapp.items_key_addrs, key)[1]))
       return state.solver.BVV(0, bitsizes.BOOL)
-    return utils.fork_guarded_has(self, mapp.items_values, key, case_has, case_not)
+    return utils.fork_guarded_has(self, mapp.values, key, case_has, case_not)
 
 # void os_map_put(struct os_map* map, void* key_ptr, uint64_t value);
-# requires mapp(map, ?key_size, ?capacity, ?items) &*&
+# requires mapp(map, ?key_size, ?capacity, ?values, ?addrs) &*&
 #          [0.25]chars(key_ptr, key_size, ?key) &*&
-#          length(items) < capacity &*&
-#          map_item_keyed(key, items) == none;
-# ensures mapp(map, key_size, capacity, ?new_items) &*&
-#         length(new_items) == length(items) + 1 &*&
-#         true == subset(items, new_items) &*&
-#         map_item_keyed(key, new_items) == some(map_item(key_ptr, key, value));
+#          length(values) < capacity &*&
+#          map_item_keyed(key, values) == none;
+# ensures mapp(map, key_size, capacity, ?new_values, ?new_addrs) &*&
+#         length(new_values) == length(values) + 1 &*&
+#         true == subset(values, new_values) &*&
+#         map_item_keyed(key, new_values) == some(value)
+#         length(new_addrs) == length(addrs) + 1 &*&
+#         true == subset(addrs, new_addrs) &*&
+#         map_item_keyed(key_ptr, new_addrs) == some(key);
 class MapPut(angr.SimProcedure):
   def run(self, map, key_ptr, value):
     # Casts
@@ -106,24 +106,27 @@ class MapPut(angr.SimProcedure):
     key = self.state.memory.load(key_ptr, mapp.key_size)
     print("!!! map put key", key)
     self.state.memory.take(25, key_ptr, mapp.key_size)
-    if utils.can_be_false(self.state.solver, self.state.maps.length(mapp.items_key_addrs).ULT(mapp.capacity)):
-      raise angr.AngrExitError("Precondition does not hold: length(items) < capacity")
-    if utils.can_be_false(self.state.solver, self.state.solver.Not(self.state.maps.get(mapp.items_key_addrs, key)[1])):
-      raise angr.AngrExitError("Precondition does not hold: map_item_keyed(key, items) == none")
+    if utils.can_be_false(self.state.solver, self.state.maps.length(mapp.values).ULT(mapp.capacity)):
+      raise angr.AngrExitError("Precondition does not hold: length(values) < capacity")
+    if utils.can_be_false(self.state.solver, self.state.solver.Not(self.state.maps.get(mapp.values, key)[1])):
+      raise angr.AngrExitError("Precondition does not hold: map_item_keyed(key, values) == none")
 
     # Postconditions
-    self.state.maps.add(mapp.items_key_addrs, key, key_ptr)
-    self.state.maps.add(mapp.items_values, key, value)
+    self.state.maps.add(mapp.values, key, value)
+    self.state.maps.add(mapp.addrs, key_ptr, key)
 
 # void os_map_erase(struct os_map* map, void* key_ptr);
-# requires mapp(map, ?key_size, ?capacity, ?items) &*&
+# requires mapp(map, ?key_size, ?capacity, ?values, ?addrs) &*&
 #          [?frac]chars(key_ptr, key_size, ?key) &*&
 #          frac != 0.0 &*&
-#          map_item_keyed(key, items) == some(map_item(key_ptr, key, _));
-# ensures mapp(map, key_size, capacity, ?new_items) &*&
-#         length(new_items) == length(items) - 1 &*&
-#         true == subset(new_items, items) &*&
-#         map_item_keyed(key, new_items) == none &*&
+#          map_item_keyed(key_ptr, addrs) == some(key);
+# ensures mapp(map, key_size, capacity, ?new_values, ?new_addrs) &*&
+#         length(new_values) == length(values) - 1 &*&
+#         true == subset(new_values, values) &*&
+#         map_item_keyed(key, new_values) == none &*&
+#         length(new_addrs) == length(addrs) - 1 &*&
+#         true == subset(new_addrs, addrs) &*&
+#         map_item_keyed(key_ptr, new_addrs) == none &*&
 #         [frac + 0.25]chars(key_ptr, key_size, key);
 class MapErase(angr.SimProcedure):
   def run(self, map, key_ptr):
@@ -136,10 +139,10 @@ class MapErase(angr.SimProcedure):
     mapp = self.state.metadata.get(Map, map)
     key = self.state.memory.load(key_ptr, mapp.key_size)
     frac = self.state.memory.take(None, key_ptr, mapp.key_size)
-    if utils.can_be_false(self.state.solver, self.state.maps.get(mapp.items_key_addrs, key)[1]) or utils.can_be_false(self.state.solver, self.state.maps.get(mapp.items_key_addrs, key)[0] == key_ptr):
-      raise angr.AngrExitError("Precondition does not hold: map_item_keyed(key, items) == some(map_item(key_ptr, key, _))")
+    if utils.can_be_false(self.state.solver, self.state.maps.get(mapp.addrs, key_ptr)[1]) or utils.can_be_false(self.state.solver, self.state.maps.get(mapp.addrs, key_ptr)[0] == key):
+      raise angr.AngrExitError("Precondition does not hold: map_item_keyed(key_ptr, addrs) == some(key)")
 
     # Postconditions
-    self.state.maps.remove(mapp.items_key_addrs, key)
-    self.state.maps.remove(mapp.items_values, key)
+    self.state.maps.remove(mapp.values, key)
+    self.state.maps.remove(mapp.addrs, key_ptr)
     self.state.memory.give(frac + 25, key_ptr, mapp.key_size)
