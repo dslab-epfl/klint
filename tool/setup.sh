d9cc1dc2dc7157242747a58c04f2dc705773d2ce
Solal Pirelli <solal.pirelli@gmail.com>
2020-07-24 10:24:17 +0000

    Improve setup.sh

--- tool/setup.sh
+++ tool/setup.sh
@@ -1,17 +1,15 @@
-#!/bin/sh
 # tested on Ubuntu 18.04
+# THIS SCRIPT SHOULD BE SOURCED NOT RAN
 
-# GCC 9 (optional; TODO move to normal gcc)
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt update
-sudo apt install gcc-9
-
-# Virtual env for angr
-sudo apt install python3-pip
-pip3 install virtualenv
-pip3 install virtualenvwrapper
-VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3 . ~/.local/bin/virtualenvwrapper.sh
-VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3 VIRTUALENVWRAPPER_VIRTUALENV=~/.local/bin/virtualenv mkvirtualenv angr
+# Virtual env for angr using pypy for python3
+sudo snap install --classic pypy3
+pypy3 -m ensurepip
+pypy3 -m pip install virtualenv virtualenvwrapper
+mkdir "$HOME/.virtualenvs"
+export WORKON_HOME="$HOME/.virtualenvs"
+export VIRTUALENVWRAPPER_PYTHON="$(which pypy3)"
+export VIRTUALENVWRAPPER_VIRTUALENV="$HOME/.local/bin/virtualenv"
+. "$HOME/.local/bin/virtualenvwrapper.sh"
+mkvirtualenv angr
 pip install -r requirements.txt
 
-# note: make sure you don't have some random z3 in your path, like one from Vigor, or it will fail
