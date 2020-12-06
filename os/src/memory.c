0af53f4064cfe79ba42268220edeabbb2e7ebb4e
Solal Pirelli <solal.pirelli@gmail.com>
2020-12-06 22:16:18 +0100

    Make OS compile again

--- os/src/memory.c
+++ os/src/memory.c
@@ -5,7 +5,7 @@
 #include "fail.h"
 
 
-void* os_memory_init(uint64_t number, uint64_t size)
+void* os_memory_alloc(uint64_t number, uint64_t size)
 {
 	void* block = calloc(number, size);
 	if (block == NULL) {
