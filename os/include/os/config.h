94acff42612c24d48775c421a728ebf5c2ebb17c
Solal Pirelli <solal.pirelli@gmail.com>
2021-01-26 16:56:38 +0100

    Fix config helper methods

--- os/include/os/config.h
+++ os/include/os/config.h
@@ -10,7 +10,7 @@ bool os_config_get(const char* name, uintmax_t* out_value);
 
 // Gets a required parameter of the given name, or exits the program if there is no such parameter or if its value is incompatible.
 #define OS_GET_U(width) \
-	uint##width##_t os_config_get_u##width(const char* name) \
+	static inline uint##width##_t os_config_get_u##width(const char* name) \
 	{ \
 		uintmax_t value; \
 		if (!os_config_get(name, &value)) { \
