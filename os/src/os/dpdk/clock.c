4de31909f100d26d685a03501e818d8baf26fdd9
Solal Pirelli <solal.pirelli@gmail.com>
2021-02-05 16:36:51 +0100

    Bring back DPDK rdtsc

--- os/src/os/dpdk/clock.c
+++ os/src/os/dpdk/clock.c
@@ -1,20 +1,14 @@
 #include "os/clock.h"
 
-#include <time.h>
-
 #include <rte_cycles.h>
 
 
-int64_t os_clock_time(void)
+uint64_t os_clock_time_ns(void)
 {
-	// DPDK has rte_rdtsc, but it must be divided by a frequency, and DPDK uses very imprecise computations to do that on old CPUs like ours
-	// So use the POSIX way instead... kind of defeats the purpose of a "DPDK" OS, but oh well, better that than a "fast" but wrong clock.
-	struct timespec tp;
-	clock_gettime(CLOCK_MONOTONIC, &tp);
-	return tp.tv_sec * 1000000000ul + tp.tv_nsec;
+	return rte_rdtsc() * 1000000000ull / rte_get_tsc_hz();
 }
 
-void os_clock_sleep_us(uint64_t microseconds)
+void os_clock_sleep_ns(uint64_t ns)
 {
-	rte_delay_us(microseconds);
+	rte_delay_us(ns * 1000);
 }
