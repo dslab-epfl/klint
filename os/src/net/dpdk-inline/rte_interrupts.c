7d1b461c467a3d3dbc465b900f6e0ee31cc25047
Solal Pirelli <solal.pirelli@gmail.com>
2021-02-13 12:19:24 +0100

    Improve dpdk-inline

--- os/src/net/dpdk-inline/rte_interrupts.c
+++ os/src/net/dpdk-inline/rte_interrupts.c
@@ -1,10 +1,31 @@
 #include <rte_interrupts.h>
 
 
+// OS ASSUMPTION: No interrupts
+
+
 int rte_intr_enable(const struct rte_intr_handle* intr_handle)
 {
 	(void) intr_handle;
+	return 0;
+}
 
-	// OS ASSUMPTION: No interrupts
+int rte_intr_disable(const struct rte_intr_handle* intr_handle)
+{
+	(void) intr_handle;
+	return 0;
+}
+
+int rte_intr_allow_others(struct rte_intr_handle* intr_handle)
+{
+	(void) intr_handle;
+	return 0;
+}
+
+int rte_intr_callback_unregister(const struct rte_intr_handle* intr_handle, rte_intr_callback_fn cb, void* cb_arg)
+{
+	(void) intr_handle;
+	(void) cb;
+	(void) cb_arg;
 	return 0;
 }
--- os/src/net/dpdk-inline/rte_malloc.c
+++ os/src/net/dpdk-inline/rte_malloc.c
@@ -1,15 +1,23 @@
 #include <rte_malloc.h>
 
+#include <rte_lcore.h>
+
 #include "os/memory.h"
 
 
-void* rte_zmalloc(const char* type, size_t size, unsigned align)
+void* rte_zmalloc_socket(const char* type, size_t size, unsigned align, int socket)
 {
 	(void) type;
+	(void) socket;
 
 	if (align > size) {
 		size = align;
 	}
 
 	return os_memory_alloc(1, size);
+}
+
+void* rte_zmalloc(const char* type, size_t size, unsigned align)
+{
+	return rte_zmalloc_socket(type, size, align, (int) rte_socket_id());
 }
