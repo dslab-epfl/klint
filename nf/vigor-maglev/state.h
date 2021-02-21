a8f56da4e9d293f4331f42b27b44b96a5d403b4e
Solal Pirelli <solal.pirelli@gmail.com>
2021-02-21 14:09:14 +0100

    Maglev refactor part 1: Inline structs.h

--- nf/vigor-maglev/state.h
+++ nf/vigor-maglev/state.h
@@ -3,12 +3,26 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#include "structs.h"
 #include "structs/map.h"
 #include "structs/pool.h"
 #include "structs/cht.h"
 
 #include "os/clock.h"
+
+struct lb_backend {
+  uint32_t ip;
+  uint16_t nic;
+  uint8_t _padding[2];
+};
+
+struct lb_flow {
+  uint32_t src_ip;
+  uint32_t dst_ip;
+  uint16_t src_port;
+  uint16_t dst_port;
+  uint8_t protocol;
+  uint8_t _padding[3];
+};
 
 
 struct State {
