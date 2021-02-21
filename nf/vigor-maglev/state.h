8f0ec6b849352c1f998bcde08f46872c680603a4
Solal Pirelli <solal.pirelli@gmail.com>
2021-02-21 14:10:21 +0100

    Maglev refactor part 2: Header-only state

--- nf/vigor-maglev/state.h
+++ nf/vigor-maglev/state.h
@@ -8,6 +8,8 @@
 #include "structs/cht.h"
 
 #include "os/clock.h"
+#include "os/memory.h"
+
 
 struct lb_backend {
   uint32_t ip;
@@ -38,4 +40,19 @@ struct State {
   size_t flow_capacity;
 };
 
-struct State* state_alloc(size_t backend_capacity, size_t flow_capacity, size_t cht_height, time_t flow_expiration_time, time_t backend_expiration_time);
+static inline struct State *state_alloc(size_t backend_capacity, size_t flow_capacity, size_t cht_height, time_t flow_expiration_time, time_t backend_expiration_time)
+{
+    // Allocate all data structures
+    struct State *state = os_memory_alloc(1, sizeof(struct State));
+    state->flow_to_flow_id = os_map_alloc(sizeof(struct lb_flow), flow_capacity);
+    state->flow_heap = os_memory_alloc(flow_capacity, sizeof(struct lb_flow));
+    state->flow_chain = os_pool_alloc(flow_capacity, flow_expiration_time);
+    state->flow_id_to_backend_id = os_memory_alloc(flow_capacity, sizeof(size_t));
+    state->ip_to_backend_id = os_map_alloc(sizeof(uint32_t), backend_capacity);
+    state->backend_ips = os_memory_alloc(backend_capacity, sizeof(uint32_t));
+    state->backends = os_memory_alloc(backend_capacity, sizeof(struct lb_backend));
+    state->active_backends = os_pool_alloc(backend_capacity, backend_expiration_time);
+    state->cht = cht_alloc(cht_height, backend_capacity);
+    state->flow_capacity = flow_capacity;
+    return state;
+}
