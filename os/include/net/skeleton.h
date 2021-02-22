edfdfd2d9e07f1889f8267aa997eb6fd409e7be8
Solal Pirelli <solal.pirelli@gmail.com>
2021-02-22 10:26:06 +0100

    Formalize net/skeleton.h contract, fixes #30

--- os/include/net/skeleton.h
+++ os/include/net/skeleton.h
@@ -5,14 +5,18 @@
 
 #include "os/config.h"
 #include "net/packet.h"
-#include "net/tx.h" // convenience for implementers
+#include "net/tx.h" // transitive include as convenience for implementers
 
 
 // Initialize any necessary state, given the number of devices; returns true iff initialization succeeded.
 bool nf_init(device_t devices_count);
+//@ requires devices_count > 0;
+//@ ensures emp;
 
 // Handles a packet
 void nf_handle(struct net_packet* packet);
+//@ requires *packet |-> _;
+//@ ensures *packet |-> _;
 
 
 // Convenience method
