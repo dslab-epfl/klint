29811b62b4efb51a33fd01910dee31aff365812d
Solal Pirelli <solal.pirelli@gmail.com>
2021-03-10 10:31:52 +0100

    Add TODO about zeroing memory

--- os/include/os/memory.h
+++ os/include/os/memory.h
@@ -15,6 +15,7 @@ typedef unsigned hash_t;
 // This allows the allocated memory's physical address to be given to a device for DMA.
 // For simplicity, never fails; if there is not enough memory available, crashes the program.
 // The contract looks a bit odd to explicitly allow for the 'alloc(1, sizeof(...))' pattern; TODO fix VeriFast to have sizeof(...) <= SIZE_MAX since sizeof is a size_t
+// TODO don't zero and add a helper for when zeroing is needed? check the perf impact
 void* os_memory_alloc(size_t count, size_t size);
 //@ requires count == 1 || count * size <= SIZE_MAX;
 //@ ensures chars(result, count * size, ?cs) &*& true == all_eq(cs, 0) &*& result + count * size <= (char*) UINTPTR_MAX;
