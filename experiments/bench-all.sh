c905cb09c94eba3a399b88d2df8766ed64c75237
Solal Pirelli <solal.pirelli@gmail.com>
2021-04-29 14:26:02 +0200

    Refresh benchmarking scripts

--- experiments/bench-all.sh
+++ experiments/bench-all.sh
@@ -1,40 +1,53 @@
 #!/bin/sh
 
 exp_dir=$(pwd)
-rm -rf "$exp_dir/results/"
+rm -rf "$exp_dir/results"
+mkdir "$exp_dir/results"
+
 cd "$exp_dir/../benchmarking"
 
-for net in tinynf dpdk; do
-  os_choices='linux'
-  if [ "$net" = 'dpdk' ]; then os_choices='linux dpdk'; fi
+# Comparison of our NFs vs Vigor-TinyNF ones
+singledir_args='--acceptableloss=0.001 --latencyload=1000 standard-single'
+for nf in bridge firewall maglev nat policer; do
+  layer=2
+  if [ "$nf" = 'maglev' ] || [ "$nf" = 'policer' ]; then layer=3; fi
+  if [ "$nf" = 'firewall' ] || [ "$nf" = 'nat' ]; then layer=4; fi
 
-  batch_choices='1'
-  if [ "$net" = 'dpdk' ]; then batch_choices='1 32'; fi
+  extra_arg=''
+  if [ "$nf" = 'maglev' ]; then extra_arg='-r'; fi # reverse heatup, i.e., make it think there are backends
 
-  for os in $os_choices; do
-    for batch in $batch_choices; do
-      mkdir -p "$exp_dir/results/$os-$net-$batch/"
-
-      for nf in vigor-nat vigor-bridge vigor-policer vigor-firewall nop; do
-        layer=4
-        if [ "$nf" = 'vigor-bridge' ]; then layer=2; fi
-        if [ "$nf" = 'vigor-policer' ]; then layer=3; fi
-
-        echo '[!!!] Benchmarking: '"$nf on $os-$net-$batch"
-        while ! BATCH_SIZE=$batch NET=$net OS=$os NF=$nf ./bench.sh .. --latencyload=-1 standard $layer ; do
-          echo '[!!!] Retrying after 5s...'
-          sleep 5
-        done
-
-        cp -r 'results' "$exp_dir/results/$os-$net-$batch/$nf"
-        cp 'bench.log' "$exp_dir/results/$os-$net-$batch/$nf/bench.log"
-      done
-    done
+  echo '[!!!] Benchmarking our NF: '"$nf"
+  while ! NF_EXT=.o CC=musl-gcc NF=$nf OS=linux NET=tinynf ./bench.sh '..' $extra_arg $singledir_args $layer ; do
+    sleep 5
   done
+  mv 'results' "$exp_dir/results/singledir-ours-$nf"
+done
+for nf in bridge fw lb nat pol; do
+  layer=2
+  if [ "$nf" = 'lb' ] || [ "$nf" = 'pol' ]; then layer=4; fi
+  if [ "$nf" = 'fw' ] || [ "$nf" = 'nat' ]; then layer=4; fi
+
+  extra_arg=''
+  if [ "$nf" = 'lb' ]; then extra_arg='-r'; fi # reverse heatup
+
+  echo '[!!!] Benchmarking Vigor-on-TinyNF NF: '"$nf"
+  while ! NF=$nf ./bench.sh "$exp_dir/baselines/tinynf" $extra_arg $singledir_args $layer ; do
+    sleep 5
+  done
+  mv 'results' "$exp_dir/results/singledir-vigor-$nf"
 done
 
-# NOTE:
-# NF_EXT=.o CC=musl-gcc NF=bridge OS=linux NET=tinynf ./bench.sh .. --acceptableloss=0.001 --latencyload=1000 standard-single 2
-# NF=bridge ./bench.sh ../experiments/baselines/tinynf/ --acceptableloss=0.001 --latencyload=1000 standard-single 2
-# for single dir
-# don't forget -r for maglev!
+# Bridge comparison
+bench_bridge()
+{
+  echo '[!!!] Benchmarking Bridge: '"$3"
+  while ! $1 ./bench.sh $2 standard 2 ; do
+    sleep 5
+  done
+  mv 'results' "$exp_dir/results/bridge-$3"
+}
+bench_bridge 'NF=bridge' "$exp_dir/baselines/fastclick" 'click'
+bench_bridge 'NF=bridge BATCH_SIZE=32 OS=dpdk NET=dpdk' '..' 'dpdk'
+bench_bridge 'NF=bridge OS=linux NET=tinynf' '..' 'ours'
+bench_bridge 'NF=vigbridge' "$exp_dir/baselines/vigor" 'vigor-dpdk'
+bench_bridge 'NF=bridge' "$exp_dir/baselines/tinynf" 'vigor-tinynf'
