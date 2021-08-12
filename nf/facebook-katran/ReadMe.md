7db193f3113ef72d8a60e68a0b18e934792c4e49
Solal Pirelli <solal.pirelli@gmail.com>
2021-08-12 09:54:19 +0200

    Revert "Remove Katran"
    This reverts commit fe25d130dce1fdc0d0fb3ae72e4136afa073dc8f.

---
+++ nf/facebook-katran/ReadMe.md
@@ -0,0 +1,3 @@
+These files, except `main.c`, `spec.py`, and `Makefile`, were obtained from the Katran project, commit b91b67d3714fa6f8507cfbf2b3a8e69e9c5999f4 of github.com/facebookincubator/katran
+They were originally in katran/lib/bpf/.
+The original files are Copyright (c) Facebook, Inc. and its affiliates, and distributed under the GPLv2 licence.
---
+++ nf/facebook-katran/main.c
@@ -0,0 +1,74 @@
+#include <stdbool.h>
+
+#define XDP_MAIN_FUNC balancer_ingress
+#define XDP_SKELETON_RESTRICT
+#include "compat/skeleton/xdp.h"
+
+#include "os/memory.h"
+
+#include "balancer_consts.h"
+
+#include "bpf.h"
+
+extern struct bpf_map_def vip_map;
+extern struct bpf_map_def lru_mapping;
+extern struct bpf_map_def fallback_cache;
+extern struct bpf_map_def ch_rings;
+extern struct bpf_map_def reals;
+extern struct bpf_map_def reals_stats;
+extern struct bpf_map_def stats;
+extern struct bpf_map_def quic_mapping;
+extern struct bpf_map_def ctl_array;
+
+#ifdef LPM_SRC_LOOKUP
+extern struct bpf_map_def lpm_src_v4;
+extern struct bpf_map_def lpm_src_v6;
+#endif
+
+#ifdef KATRAN_INTROSPECTION
+extern struct bpf_map_def lpm_src_v4;
+#endif
+
+#ifdef INLINE_DECAP_GENERIC
+extern struct bpf_map_def lpm_src_v4;
+extern struct bpf_map_def lpm_src_v4;
+#endif
+
+#ifdef GUE_ENCAP
+extern struct bpf_map_def lpm_src_v4;
+#endif
+
+bool nf_init(device_t devices_count)
+{
+	(void) devices_count;
+
+	bpf_map_init(&vip_map, true);
+	bpf_map_init(&lru_mapping, false); // is a map of maps; will lead to using fallback_cache as default
+	bpf_map_init(&fallback_cache, true); // havoc the cache so it looks like one of the maps that userspace put in lru_mapping
+	bpf_map_init(&ch_rings, true);
+	bpf_map_init(&reals, true);
+	bpf_map_init(&reals_stats, true);
+	bpf_map_init(&stats, true);
+	bpf_map_init(&quic_mapping, true);
+	bpf_map_init(&ctl_array, true);
+
+#ifdef LPM_SRC_LOOKUP
+	bpf_map_init(&lpm_src_v4, true);
+	bpf_map_init(&lpm_src_v6, true);
+#endif
+
+#ifdef KATRAN_INTROSPECTION
+	bpf_map_init(&event_pipe, true);
+#endif
+
+#ifdef INLINE_DECAP_GENERIC
+	bpf_map_init(&decap_dst, true);
+	bpf_map_init(&subprograms, true);
+#endif
+
+#ifdef GUE_ENCAP
+	bpf_map_init(&pckt_srcs, true);
+#endif
+
+	return true;
+}
