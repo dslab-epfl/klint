fd447cb4eead2bcbf4d531489bacf6c69a8fb227
Solal Pirelli <solal.pirelli@gmail.com>
2021-09-03 13:55:18 +0000

    Ephemeral packet allocation for libNF

--- tool/klint/externals/net/packet.py
+++ tool/klint/externals/net/packet.py
@@ -1,18 +1,16 @@
 import angr
-import archinfo
 import claripy
 from collections import namedtuple
 
 from kalm import clock
 from kalm import utils
 
-PACKET_MIN = 64 # the NIC will pad it if shorter
+PACKET_MIN = 60 # the NIC will pad it if shorter
 PACKET_MTU = 1514 # 1500 (Ethernet spec) + 2xMAC + EtherType
 
 NetworkMetadata = namedtuple('NetworkMetadata', ['received_addr', 'received_device', 'received_length', 'transmitted'])
 
 # For the packet layout, see the C header (not reproducing here to avoid getting out of sync with changes)
-# TODO use angr.types instead and just parse it...
 def packet_size(state):
     return state.sizes.ptr + state.sizes.size_t + state.sizes.uint64_t + state.sizes.uint16_t
 
@@ -30,14 +28,11 @@ def alloc(state, devices_count):
     packet_addr = state.heap.allocate(1, packet_size(state) // 8, name="packet")
     packet_length = claripy.BVS("packet_length", state.sizes.size_t)
     state.solver.add(packet_length.UGE(PACKET_MIN), packet_length.ULE(PACKET_MTU))
-    # The packet is an array of bytes, instead of an array with a single long cell,
-    # so that state merging can refer to different parts of the packet in a single expression
-    # Allocate 3*MTU so that BPF's adjust_head can adjust positively or negatively
-    data_addr = state.heap.allocate(3 * PACKET_MTU, 1, name="packet_data") # TODO: allocate with packet_length size instead; find another way to deal with BPF's adjust_head
+    data_addr = state.heap.allocate(packet_length, 1, ephemeral=True, name="packet_data")
     packet_device = claripy.BVS("packet_device", state.sizes.uint16_t)
     state.solver.add(packet_device.ULT(devices_count))
     (packet_time, _) = clock.get_time_and_cycles(state)
-    packet_data = packet_device.concat(packet_time).concat(packet_length).concat(data_addr + PACKET_MTU)
+    packet_data = packet_device.concat(packet_time).concat(packet_length).concat(data_addr)
     state.memory.store(packet_addr, packet_data, endness=state.arch.memory_endness)
     state.metadata.append(None, NetworkMetadata(data_addr, packet_device, packet_length, []))
     return packet_addr
--- tool/klint/externals/net/tx.py
+++ tool/klint/externals/net/tx.py
@@ -7,8 +7,6 @@ from . import packet
 
 TransmissionMetadata = namedtuple("TransmissionMetadata", ["data_addr", "length", "flags", "is_flood", "device", "excluded_devices"])
 
-# TODO enforce model where it's the "right" buffer we're sending and enforce the NF doesn't touch it afterwards
-
 # void net_transmit(struct net_packet* packet, device_t device, enum net_transmit_flags flags);
 class net_transmit(angr.SimProcedure):
     def run(self, pkt, device, flags):
@@ -21,8 +19,6 @@ class net_transmit(angr.SimProcedure):
 
         metadata = self.state.metadata.get_one(packet.NetworkMetadata)
         metadata.transmitted.append(TransmissionMetadata(data_addr, length, flags, False, device, None))
-
-        #self.state.heap.take(None, data_addr - packet.PACKET_MTU)
 
 # void net_flood(struct net_packet* packet, enum net_transmit_flags flags);
 class net_flood(angr.SimProcedure):
@@ -37,8 +33,6 @@ class net_flood(angr.SimProcedure):
         metadata = self.state.metadata.get_one(packet.NetworkMetadata)
         metadata.transmitted.append(TransmissionMetadata(data_addr, length, flags, True, device, None))
 
-        #self.state.heap.take(None, data_addr - packet.PACKET_MTU)
-
 # void net_flood_except(struct net_packet* packet, bool* disabled_devices, enum net_transmit_flags flags);
 class net_flood_except(angr.SimProcedure):
     def run(self, pkt, disabled_devices, flags):
@@ -52,5 +46,3 @@ class net_flood_except(angr.SimProcedure):
 
         metadata = self.state.metadata.get_one(packet.NetworkMetadata)
         metadata.transmitted.append(TransmissionMetadata(data_addr, length, flags, True, device, disabled_devices))
-
-        #self.state.heap.take(None, data_addr - packet.PACKET_MTU)
