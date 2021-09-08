94320b025bff55293b7aae617844dfc1d85d4e12
Solal Pirelli <solal.pirelli@gmail.com>
2021-09-08 12:38:35 +0200

    Use python, not pypy, for setup.sh

--- tool/setup.sh
+++ tool/setup.sh
@@ -1,17 +1,15 @@
-# tested on Ubuntu 18.04
+# tested on Ubuntu 18.04 and 20.04
 # THIS SCRIPT SHOULD BE SOURCED NOT RAN
-# TODO fail if the script is not sourced (how?)
 
 if [ ! -d "$HOME/.virtualenvs/angr" ]; then
-  # Virtual env for angr using pypy for python3
-  sudo snap install --classic pypy3
-  pypy3 -m ensurepip
-  pypy3 -m pip install virtualenv virtualenvwrapper
+  # Virtual env for angr using python3
+  sudo apt install -y python3 python3-pip
+  python3 -m pip install virtualenv virtualenvwrapper
   mkdir -p "$HOME/.virtualenvs"
 fi
 
 export WORKON_HOME="$HOME/.virtualenvs"
-export VIRTUALENVWRAPPER_PYTHON="$(which pypy3)"
+export VIRTUALENVWRAPPER_PYTHON="$(which python3)"
 export VIRTUALENVWRAPPER_VIRTUALENV="$HOME/.local/bin/virtualenv"
 . "$HOME/.local/bin/virtualenvwrapper.sh"
 
