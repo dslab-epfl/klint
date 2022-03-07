40173d37b8f4408ffc1df263ef833d39ae7a2529
Solal Pirelli <solal.pirelli@gmail.com>
2022-03-07 12:02:48 +0100

    Fixes for experiments after refactoring

--- experiments/bench-all.sh
+++ experiments/bench-all.sh
@@ -12,9 +12,9 @@ bridge_args='standard 2'
 echo '[!!!] Benchmarking bridges'
 while ! NF=bridge BATCH_SIZE=32 ./bench.sh "$exp_dir/baselines/fastclick" $bridge_args ; do sleep 5; done
 mv 'results' "$exp_dir/results/bridge-click"
-while ! NF=bridge OS=dpdk NET=dpdk BATCH_SIZE=32 ./bench.sh '../env' $bridge_args ; do sleep 5; done
+while ! NF=../nf/bridge OS=dpdk NET=dpdk BATCH_SIZE=32 ./bench.sh '../env' $bridge_args ; do sleep 5; done
 mv 'results' "$exp_dir/results/bridge-dpdk"
-while ! NF=bridge NF_EXT=.o CC=musl-gcc OS=linux NET=tinynf ./bench.sh '../env' $bridge_args ; do sleep 5; done
+while ! NF=../nf/bridge NF_EXT=.o CC=musl-gcc OS=linux NET=tinynf ./bench.sh '../env' $bridge_args ; do sleep 5; done
 mv 'results' "$exp_dir/results/bridge-ours"
 while ! NF=vigbridge ./bench.sh "$exp_dir/baselines/vigor" $bridge_args ; do sleep 5; done
 mv 'results' "$exp_dir/results/bridge-vigor-dpdk"
@@ -32,7 +32,7 @@ for nf in bridge firewall maglev nat policer; do
   if [ "$nf" = 'maglev' ]; then extra_arg='--maglev'; fi
 
   echo '[!!!] Benchmarking our NF: '"$nf"
-  while ! NF_EXT=.o CC=musl-gcc NF=$nf OS=linux NET=tinynf ./bench.sh '../env' $extra_arg $singledir_args $layer ; do
+  while ! NF_EXT=.o CC=musl-gcc NF=../nf/$nf OS=linux NET=tinynf ./bench.sh '../env' $extra_arg $singledir_args $layer ; do
     sleep 5
   done
   mv 'results' "$exp_dir/results/singledir-ours-$nf"
